75f09771fb7388f05a42877c9ca16e2586d6f0b1
IvanR96 <irios96@hotmail.com>
2025-11-03 22:42:30 -0800

    react app folder

---
+++ my-react-app/src/main.jsx
@@ -0,0 +1,10 @@
+
+import { createRoot } from 'react-dom/client'
+import './index.css'
+
+createRoot(document.getElementById('root')).render(
+  <div>
+    <h1>Hello World!</h1>
+    <p>This is a paragraph</p>
+  </div>,
+)
